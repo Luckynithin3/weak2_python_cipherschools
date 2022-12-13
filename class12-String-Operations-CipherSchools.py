a7c873c29a1ddf76fa1e81192ce87a8d8916b6a6
Nithin bhavani prasad dora <115680014+Luckynithin3@users.noreply.github.com>
2022-12-13 21:17:38 +0530

    week2

---
+++ class12-String-Operations-CipherSchools.py
@@ -0,0 +1,31 @@
+a=5
+print("value of a is %d" %(a))
+
+
+a,b,c,d =1,2,3,4
+"a={},b={},c={}".format(c,b,a)
+
+
+a,b,c,d =1,2,3,4
+"a={2},b={1},c={0}".format(c,b,a)
+
+
+name="Dinesh"
+company="Google"
+
+"name={name} company={company}".format(name=name,company=company)
+
+
+print(f"name={name}")
+
+
+"       Dinesh        ".strip()
+
+
+"1,2,3,4,5".split(",")
+
+
+"Dinesh".replace("a","z")
+
+
+"Dinesh".count("a")
